6bfb72972b4819fc18abefffe139fbd376813eff
Ostap Sulyk <60716383+Ostap-Sulyk@users.noreply.github.com>
2023-04-03 10:23:17 -0400

    functionality is ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MissionListComponent } from './missionlist/mission-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MissionListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/missionlist/mission-list.component.ts
+++ src/app/missionlist/mission-list.component.ts
@@ -7,12 +7,36 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./mission-list.component.css']
 })
 export class MissionListComponent {
-  launches: any[] = [];
-  ngOnInit(): void {
-    this.http.get('https://api.spacexdata.com/v3/launches').subscribe(data => {
-      this.launches = data as any[];
-    });
-  }
+  launches: Mission[] = [];
+  filteredMissions: Mission[] = [];
+  filterYear: string = "";
   constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    this.http.get<Mission[]>('https://api.spacexdata.com/v3/launches?limit=100').subscribe((data: Mission[]) => {
+      this.launches = data.map((mission) => {
+        return {
+          ...mission,
+          // @ts-ignore
+          mission_patch_small: mission.links.mission_patch_small
+        };
+      });
+      this.filteredMissions = this.launches;
+    });
+  }
+  applyFilter() {
+    if (!this.filterYear) {
+      this.filteredMissions = this.launches;
+      return;
+    }
+    this.filteredMissions = this.launches.filter((mission) => {
+      return mission.launch_year.toString().includes(this.filterYear);
+    });
+  }
+}
+export interface Mission {
+  mission_name: string;
+  launch_year: string;
+  details: string;
+  mission_patch_small: string;
 }
